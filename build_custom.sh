59bd038f8bc2b910a17221c8a8286378abd87493
Johannes Schauer Marin Rodrigues <josch@mister-muffin.de>
2023-10-26 00:42:50 +0200

    build_custom.sh: remove more blender leftovers

--- build_custom.sh
+++ build_custom.sh
@@ -8,36 +8,6 @@ set -u
 
 . ./common.sh
 
-# starting with 2.80, blender requires OpenGL 3.2+, so 2.79b is the last one
-# that works on reform with imx8mq
-#
-# currently, building blender fails to builds with:
-#
-#     In file included from /usr/include/openvdb/tools/LevelSetRebuild.h:11,
-#                      from /usr/include/openvdb/tools/GridTransformer.h:16,
-#                      from /usr/include/openvdb/tools/Clip.h:16,
-#                      from /<<PKGBUILDDIR>>/intern/openvdb/intern/openvdb_dense_convert.h:34,
-#                      from /<<PKGBUILDDIR>>/intern/openvdb/openvdb_capi.cc:27:
-#     /usr/include/openvdb/tools/VolumeToMesh.h:21:10: fatal error: tbb/task_scheduler_init.h: No such file or directory
-#        21 | #include <tbb/task_scheduler_init.h>
-#           |          ^~~~~~~~~~~~~~~~~~~~~~~~~~~
-#     compilation terminated.
-#
-# This is because tbb dropped the tbb/task_scheduler_init.h header. Fixing this
-# has to happen in openvdb but the proper fix will be to add support for onetbb
-# to openvdb because tbb developers changed the name to onetbb:
-# https://github.com/oneapi-src/oneTBB
-# Tracking bug in Debian:
-# https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=1011215
-# OpenVDB bug:
-# https://github.com/AcademySoftwareFoundation/openvdb/issues/1366
-#if [ -z "$(reprepro listfilter reform "\$Source (== blender)")" ]; then
-#	env --chdir=blender \
-#		BUILD_ARCH="$BUILD_ARCH" HOST_ARCH="$HOST_ARCH" \
-#		BASESUITE="$BASESUITE" OURSUITE="$OURSUITE" \
-#		./build.sh
-#fi
-
 # simplified version of $our_version from build_patched.sh because the binary
 # version is always equal to the source version here
 our_version=$(reprepro --list-format '${version}\n' -T deb listfilter "$OURSUITE" "\$Source (== reform-tools)" | uniq)
