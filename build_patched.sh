655756f28d9dbc609eab4cbcbceb597221235cb6
Johannes Schauer Marin Rodrigues <josch@mister-muffin.de>
2023-12-12 17:05:04 +0100

    build_patched.sh: fix build log architecture

--- build_patched.sh
+++ build_patched.sh
@@ -89,7 +89,7 @@ for p in patches/*; do
 				--extra-repository="$SRC_LIST_PATCHED"
 			mv -v ../*.changes "../${p}_native.changes"
 			dcmd mv -v ../*.changes "$ROOTDIR/changes/"
-			mv -v ../*_$HOST_ARCH-*.build "$ROOTDIR/buildlogs"
+			mv -v ../*_$BUILD_ARCH-*.build "$ROOTDIR/buildlogs"
 		fi
 		cd ..
 	)
