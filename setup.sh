9a4728ffb285336bb2d4596daa53ffb147fb53b8
Johannes Schauer Marin Rodrigues <josch@mister-muffin.de>
2023-12-12 14:51:50 +0100

    setup.sh: special handling for BASESUITE being stable-backports and experimental

--- setup.sh
+++ setup.sh
@@ -19,6 +19,21 @@ Components: main
 UDebComponents: main
 Description: updated packages for mnt reform
 EOF
+	# if OURSUITE is backports, also add the base suite
+	case $OURSUITE in
+		*-backports)
+			cat >> "$REPREPRO_BASE_DIR/conf/distributions" <<EOF
+
+Codename: ${OURSUITE%-backports}
+Label: $OURLABEL
+Suite: ${OURSUITE%-backports}
+Architectures: $HOST_ARCH $(test "$BUILD_ARCH" = "$HOST_ARCH" || echo "$BUILD_ARCH")
+Components: main
+UDebComponents: main
+Description: updated packages for mnt reform
+EOF
+		;;
+	esac
 	cat > "$REPREPRO_BASE_DIR/conf/options" <<EOF
 verbose
 EOF
@@ -29,10 +44,26 @@ if [ ! -d "$chdistdata" ]; then
 	chdist_base create
 fi
 
-cat << END > "$chdistdata/base/etc/apt/sources.list"
-deb-src $MIRROR $BASESUITE main
-deb-src $MIRROR experimental main
-END
+{
+echo "deb-src $MIRROR $BASESUITE main";
+case $BASESUITE in
+	experimental)
+		echo "deb-src $MIRROR unstable main"
+		;;
+	unstable) : ;;
+	*-backports)
+		echo "deb-src $MIRROR ${BASESUITE%-backports} main"
+		echo "deb-src $MIRROR ${BASESUITE%-backports}-updates main"
+		echo "deb-src http://security.debian.org/debian-security ${BASESUITE%-backports}-security main"
+		;;
+	*)
+		# assume this is a stable release
+		echo "deb-src $MIRROR $BASESUITE-updates main"
+		echo "deb-src http://security.debian.org/debian-security $BASESUITE-security main"
+		;;
+esac;
+} > "$chdistdata/base/etc/apt/sources.list"
+
 chdist_base apt-get update
 
 mkdir -p changes buildlogs
